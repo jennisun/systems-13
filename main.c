fdb8aa0dadad6bce65d6318f2e4f422874ceae81
jennisun <jsun20@stuy.edu>
2021-11-07 14:01:39 -0500

    wrote read_data()

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 struct pop_entry {
   int year;
@@ -55,7 +56,7 @@ void read_csv(char * name) {
   struct pop_entry arr[lines * 5];
   char *borough[5] = {"Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island"};
 
-  for (i = 1; i < lines; i ++) {
+  for (i = 1; i <= lines; i ++) {
     char temp[bytes[i] - bytes[i - 1]];
     result = read(file1, temp, sizeof(temp));
 
@@ -67,7 +68,7 @@ void read_csv(char * name) {
       arr[index].year = hold[0];
       arr[index].population = hold[j];
       strcpy(arr[index].boro, borough[j - 1]);
-      // printf("%d\t%d\t%s\n", arr[index].year, arr[index].population, arr[index].boro);
+      printf("%d\t%d\t%s\n", arr[index].year, arr[index].population, arr[index].boro);
     }
   }
 
@@ -84,8 +85,32 @@ void read_csv(char * name) {
   }
 }
 
+void read_data() {
+  int size = filesize("nyc_pop.data");
+  int file = open("nyc_pop.data", O_RDONLY);
+  if (file < 0) {
+    printf("%s\n", strerror(errno));
+    return;
+  }
+
+  struct pop_entry *arr = malloc(file);
+  int result = read(file, arr, size);
+  if (result < 0) {
+    printf("%s\n", strerror(errno));
+    return;
+  }
+
+  int i = 0;
+  struct pop_entry hi;
+  for (i = 0; i < size / sizeof(hi); i ++) {
+    // printf("%d:\t year: %d\t boro: %s\t pop: %d\n", i, arr[i].year, arr[i].boro, arr[i].population);
+    printf("Entry %d\tYear: %d\tPopulation: %d\tBorough: %s\n", i, arr[i].year, arr[i].population, arr[i].boro);
+  }
+}
+
 int main() {
-  read_csv("data.txt");
+  // read_csv("data.txt");
+  read_data();
 
   return 0;
 }
